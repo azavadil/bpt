9c836dc931997e072a050451712a082351d39b59
Anthony Zavadil <anthony.zavadil@gmail.com>
2013-10-02 15:59:13 +0000

    add ~/public/js/views/placebet.js

---
+++ public/js/views/placebet.js
@@ -0,0 +1,39 @@
+define(['SocialNetView', 
+	'text!templates/placebet.html'], 
+       function(SocialNetView, 
+		placebetTemplate) {
+	   
+
+	   var PlaceBetView = SocialNetView.extend({
+	       
+	       requireLogin: false,
+
+	       el: $('#content'),
+
+	       events: {
+		   "submit form": "placebet"
+	       },
+
+	       placebet: function() {
+		   $.post('/placebet', {
+		       username: $('input[name=counterparty]').val(),
+		       betDescription: $('input[name=description]').val(),
+		       referenceIndex: $('input[name=refIndex'].val(), 
+		       terminationEvent: $('input[name=terminationEvent]').val(),
+		       authorBet: $('input[name=thisBet]').val(), 
+		       counterpartyBet: $('input[name=counterpartyBet]').val()
+		       
+		   }, function(data) {
+		       console.log(data);
+		       window.location.hash = 'index';  //profile is probably betterdd
+		   });
+		   return false;
+	       },
+
+	       render: function() {
+		   this.$el.html(placebetTemplate);
+	       }
+	   });
+	   
+	   return PlaceBetView;
+});
