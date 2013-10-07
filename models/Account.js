ad001a659509cab595d10a91cab067e1951990b8
Anthony Zavadil <anthony.zavadil@gmail.com>
2013-10-07 15:21:31 +0000

    refactor Bet as separate database, ~/models/Accounts.js changed

--- models/Account.js
+++ models/Account.js
@@ -4,7 +4,7 @@ module.exports = function(config, mongoose, nodemailer) {
 
 
 
-    var Bet = new mongoose.Schema({
+    var BetSchema = new mongoose.Schema({
       authorId: { type: mongoose.Schema.Types.ObjectId },
       counterpartyId: { type: mongoose.Schema.Types.ObjectId },
       authorName: { type: String }, 
@@ -22,84 +22,85 @@ module.exports = function(config, mongoose, nodemailer) {
       winner: {type: mongoose.Schema.ObjectId },
       added:     { type: Date },     // When the bet was added
       updated:   { type: Date }      // When the bet last updated
-  });
+    });
 
-  var AccountSchema = new mongoose.Schema({
-    email:     { type: String },
-    password:  { type: String },
-    username: { type: String, unique: true }, 
-    numCompleteBets : {type: Number }, 
-    numWinningBets : {type: Number }, 
-    winningPercentage : { type: Number }, 
-    accountBal: { type: Number },       
-    bets:  [Bet]
-  });
+    var AccountSchema = new mongoose.Schema({
+	email:     { type: String },
+	password:  { type: String },
+	username: { type: String, unique: true }, 
+	numCompleteBets : {type: Number }, 
+	numWinningBets : {type: Number }, 
+	winningPercentage : { type: Number }, 
+	accountBal: { type: Number },       
+	bets:  [String]
+    });
 
     var Account = mongoose.model('Account', AccountSchema);
     
+    var Bet = mongoose.model('Bet', BetSchema); 
     
     
-  var registerCallback = function(err) {
-    if (err) {
-      return console.log(err);
+    var registerCallback = function(err) {
+	if (err) {
+	    return console.log(err);
+	};
+	return console.log('Account was created');
     };
-    return console.log('Account was created');
-  };
 
-  var changePassword = function(accountId, newpassword) {
-    var shaSum = crypto.createHash('sha256');
-    shaSum.update(newpassword);
-    var hashedPassword = shaSum.digest('hex');
-    Account.update({_id:accountId}, {$set: {password:hashedPassword}},{upsert:false},
-      function changePasswordCallback(err) {
-        console.log('Change password done for account ' + accountId);
-    });
-  };
+    var changePassword = function(accountId, newpassword) {
+	var shaSum = crypto.createHash('sha256');
+	shaSum.update(newpassword);
+	var hashedPassword = shaSum.digest('hex');
+	Account.update({_id:accountId}, {$set: {password:hashedPassword}},{upsert:false},
+		       function changePasswordCallback(err) {
+			   console.log('Change password done for account ' + accountId);
+		       });
+    };
 
-  var forgotPassword = function(email, resetPasswordUrl, callback) {
-    var user = Account.findOne({email: email}, function findAccount(err, doc){
-      if (err) {
-        // Email address is not a valid user
-        callback(false);
-      } else {
-        var smtpTransport = nodemailer.createTransport('SMTP', config.mail);
-        resetPasswordUrl += '?account=' + doc._id;
-        smtpTransport.sendMail({
-          from: 'thisapp@example.com',
-          to: doc.email,
-          subject: 'SocialNet Password Request',
-          text: 'Click here to reset your password: ' + resetPasswordUrl
-        }, function forgotPasswordResult(err) {
-          if (err) {
-            callback(false);
-          } else {
-            callback(true);
-          }
-        });
-      }
-    });
-  };
+    var forgotPassword = function(email, resetPasswordUrl, callback) {
+	var user = Account.findOne({email: email}, function findAccount(err, doc){
+	    if (err) {
+		// Email address is not a valid user
+		callback(false);
+	    } else {
+		var smtpTransport = nodemailer.createTransport('SMTP', config.mail);
+		resetPasswordUrl += '?account=' + doc._id;
+		smtpTransport.sendMail({
+		    from: 'thisapp@example.com',
+		    to: doc.email,
+		    subject: 'SocialNet Password Request',
+		    text: 'Click here to reset your password: ' + resetPasswordUrl
+		}, function forgotPasswordResult(err) {
+		    if (err) {
+			callback(false);
+		    } else {
+			callback(true);
+		    }
+		});
+	    }
+	});
+    };
 
-  var login = function(email, password, callback) {
-    var shaSum = crypto.createHash('sha256');
-    shaSum.update(password);
-    Account.findOne({email:email,password:shaSum.digest('hex')},function(err,doc){
-      callback(doc);
-    });
-  };
+    var login = function(email, password, callback) {
+	var shaSum = crypto.createHash('sha256');
+	shaSum.update(password);
+	Account.findOne({email:email,password:shaSum.digest('hex')},function(err,doc){
+	    callback(doc);
+	});
+    };
 
-  var allBettors = function( callback ){ 
-      Account.find({}, {"username": 1, "accountBal": 1, "winningPercentage": 1}).exec( callback ); 
-  } ;
+    var allBettors = function( callback ){ 
+	Account.find({}, {"username": 1, "accountBal": 1, "winningPercentage": 1}).exec( callback ); 
+    } ;
 
-  var find20Bettors = function( blockNumber, callback ){ 
-      var blockSize = 20; 
-      Account.find({}, {"username": 1, "accountBal": 1, "winningPercentage": 1})
-	  .limit(20)
-	  .skip(blockSize*blockNumber)
-	  .sort({"accountBal":-1})
-	  .exec(callback); 
-  };
+    var find20Bettors = function( blockNumber, callback ){ 
+	var blockSize = 20; 
+	Account.find({}, {"username": 1, "accountBal": 1, "winningPercentage": 1})
+	    .limit(20)
+	    .skip(blockSize*blockNumber)
+	    .sort({"accountBal":-1})
+	    .exec(callback); 
+    };
 
 
     var findByString = function(searchStr, callback) {
@@ -110,7 +111,7 @@ module.exports = function(config, mongoose, nodemailer) {
 	    } else { 
 		// code here callback
 	    }
-j	});
+	});
     };
 
     var findCounterparty = function(searchStr, callback){
@@ -130,11 +131,11 @@ j	});
     }; 
 
 
-  var findById = function(accountId, callback) {
-    Account.findOne({_id:accountId}, function(err,doc) {
-      callback(doc);
-    });
-  };
+    var findById = function(accountId, callback) {
+	Account.findOne({_id:accountId}, function(err,doc) {
+	    callback(doc);
+	});
+    };
 
   
     var findBetById = function(betId, callback) {
@@ -149,62 +150,42 @@ j	});
 	});
     };
 
-  var addBet = function(account, addBet) {
-    bet = {
-      authorId: addBet.accountId, 
-      counterpartyId: addBet.counterpartyId, 
-      authorBet: addBet.authorBet, 
-      counterpartyBet: addBet.counterpartyBet, 
-      wagerDescription: addBet.wagerDescription, 
-      referenceIndex: addBet.referenceIndex,
-      terminationEvent: addBet.terminationEvent, 
-      added: new Date(),
-      updated: new Date()
-    };
-    account.bets.push( bet );
-
-    account.save(function (err) {
-      if (err) {
-        console.log('Error saving account: ' + err);
-      }
-    });
-  };
-
  
-  var hasContact = function(account, contactId) {
-    if ( null == account.contacts ) return false;
+ 
+    var hasContact = function(account, contactId) {
+	if ( null == account.contacts ) return false;
 
-    account.contacts.forEach(function(contact) {
-      if ( contact.accountId == contactId ) {
-        return true;
-      }
-    });
-    return false;
-  };
+	account.contacts.forEach(function(contact) {
+	    if ( contact.accountId == contactId ) {
+		return true;
+	    }
+	});
+	return false;
+    };
 
-  var register = function(username, email, password) {
-    var shaSum = crypto.createHash('sha256');
-    shaSum.update(password);
+    var register = function(username, email, password) {
+	var shaSum = crypto.createHash('sha256');
+	shaSum.update(password);
 
-    console.log('Registering ' + email);
-    var user = new Account({
-	username: username,
-	email: email,
-	password: shaSum.digest('hex'),      
-	numCompleteBets: 0, 
-	numWinningBets: 0, 
-	winningPercentage: 0, 
-	accountBal: 1000
-    });
-    user.save(registerCallback);
-    console.log('Save command was sent');
-  };
+	console.log('Registering ' + email);
+	var user = new Account({
+	    username: username,
+	    email: email,
+	    password: shaSum.digest('hex'),      
+	    numCompleteBets: 0, 
+	    numWinningBets: 0, 
+	    winningPercentage: 0, 
+	    accountBal: 1000
+	});
+	user.save(registerCallback);
+	console.log('Save command was sent');
+    };
 
     var placeBet = function(account, betDetails){
 
 	console.log('~/models/Account.js | placebet | authorId'); 
 	
-	var bet = {
+	var bet = new Bet({
 	    authorId: betDetails.authorId, 
 	    counterpartyId: betDetails.counterpartyId,
 	    authorName: betDetails.authorName, 
@@ -222,13 +203,19 @@ j	});
 	    winner: null, 
 	    added: new Date(), 
 	    updated: new Date()
-	}; 
+	}); 
 
-	account.bets.push(bet); 
-	account.save(function(err){
-	    if(err){
-		console.log('Error saving bet: ' + err); 
-	    }
+	bet.save( function( err ){ 
+	    if( err ) { 
+		console.log('Error saving bet: '+ err ); 
+	    } 
+	    
+	    account.bets.push( bet.id ); 
+	    account.save( function ( err ) { 
+		if ( err ) { 
+		    console.log('Error pushing bet to account and saving' + err); 
+		}
+	    }); 
 	}); 
     }; 
 		    
